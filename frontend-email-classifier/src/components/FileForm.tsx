25e87e59a92c3d18309663e2a4c0bbec4cb23978
Kaique Ivo <kaiqueivo1011@gmail.com>
2025-08-30 15:54:14 -0300

    Fix: unused import

--- frontend-email-classifier/src/components/FileForm.tsx
+++ frontend-email-classifier/src/components/FileForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useState } from "react";
 import { styled } from '@mui/material/styles';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
